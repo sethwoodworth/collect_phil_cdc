87faadaa75197f618126bca3f3f50f9968480305
Parker Phinney <parker.phinney@gmail.com>
2010-01-02 21:24:21 -0800

    cookie handling is now more clever, saving 2/3 of html scraping http requests

--- scraper.py
+++ scraper.py
@@ -1,11 +1,51 @@
 from BeautifulSoup import BeautifulSoup
+import string
 import urllib
 import urllib2
 import os.path
 import cookielib
 
+def get_me_a_cookie():
+	# this post data doesn't seem to really matter, as long as it is correctly formed
+	quicksearch_page_post_values = {
+		'formaction':	'SEARCH',
+		'illustrations':	'on',
+		'keywords':	'liver',
+		'keywordstext':	'liver',
+		'photos':	'on',
+		'searchtype':	'photo|illustration|video',
+		'video':	'on',
+	}
+	urlopen = urllib2.urlopen
+	Request = urllib2.Request
+	cj = cookielib.LWPCookieJar()
+	# This is a subclass of FileCookieJar
+	# that has useful load and save methods
 
-def cdc_phil_scrape(id):
+	# Now we need to get our Cookie Jar
+	# installed in the opener;
+	# for fetching URLs
+	# we get the HTTPCookieProcessor
+	# and install the opener in urllib2
+	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+	urllib2.install_opener(opener)
+	# fake a user agent, some websites (like google) don't like automated exploration
+	txheaders =  {'User-agent' : 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}
+
+	# we have to step through the landing page and the search results pages in order to access an individual image's page
+	# otherwise the site gives us session errors
+	# so we go ahead and do that, picking up the necessary cookies along the way
+	req = Request('http://phil.cdc.gov/phil/home.asp', None, txheaders)
+	#cj.save(COOKIEFILE)                     # save the cookies 
+	handle = urlopen(req)
+	req = Request('http://phil.cdc.gov/phil/quicksearch.asp', urllib.urlencode(quicksearch_page_post_values), txheaders)
+	#cj.save(COOKIEFILE)                     # save the cookies again
+	handle = urlopen(req)
+	
+	print "got a new cookie."
+	return cj
+
+def cdc_phil_scrape(id, cj=get_me_a_cookie()):
 	url_to_scrape = 'http://phil.cdc.gov/phil/details.asp';
 	# this post data doesn't seem to really matter, as long as it is correctly formed
 	image_page_post_values = {'count':	'1',
@@ -24,30 +64,14 @@ def cdc_phil_scrape(id):
 		'returnpage':	'quicksearch.asp',
 		'searchtype':	'all',
 	}
-	quicksearch_page_post_values = {
-		'formaction':	'SEARCH',
-		'illustrations':	'on',
-		'keywords':	'liver',
-		'keywordstext':	'liver',
-		'photos':	'on',
-		'searchtype':	'photo|illustration|video',
-		'video':	'on',
-	}
 
 	urlopen = urllib2.urlopen
 	Request = urllib2.Request
-	cj = cookielib.LWPCookieJar()
-	# This is a subclass of FileCookieJar
-	# that has useful load and save methods
+	if not cj:
+		print "making a new cookie jar."
+		print "really, this shouldn't be happening.  beware."
+		cj = get_me_a_cookie()
 
-	# FIXME: this probably isn't necessary. commenting out for now
-
-	#COOKIEFILE = 'cookies.lwp'
-	## the path and filename to save your cookies in
-	#if os.path.isfile(COOKIEFILE):
-	## if we have a cookie file already saved
-	## then load the cookies into the Cookie Jar
-	#	cj.load(COOKIEFILE)
 
 	# Now we need to get our Cookie Jar
 	# installed in the opener;
@@ -62,22 +86,19 @@ def cdc_phil_scrape(id):
 	# fake a user agent, some websites (like google) don't like automated exploration
 	txheaders =  {'User-agent' : 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}
 
-	# we have to step through the landing page and the search results pages in order to access an individual image's page
-	# otherwise the site gives us session errors
-	# so we go ahead and do that, picking up the necessary cookies along the way
-	req = Request('http://phil.cdc.gov/phil/home.asp', None, txheaders)
-	#cj.save(COOKIEFILE)                     # save the cookies 
-	handle = urlopen(req)
-	req = Request('http://phil.cdc.gov/phil/quicksearch.asp', urllib.urlencode(quicksearch_page_post_values), txheaders)
-	#cj.save(COOKIEFILE)                     # save the cookies again
-	handle = urlopen(req)
-
 	# finally, we can grab the actual image's page
 	req = Request(url_to_scrape, urllib.urlencode(image_page_post_values), txheaders)
 	handle = urlopen(req)
 
-	return handle.read() #returns the page
+	html = handle.read() #returns the page
+	return html
 
+
+def is_session_expired_page(html):
+	if string.count("Your session information is no longer valid. ", html) == 0:
+		return False
+	else:
+		return True
 
 
 if __name__ == '__main__':
--- start.py
+++ start.py
@@ -18,9 +18,16 @@ def mkdir(dirname):
 # run this before downloading hires images
 def bootstrap_filestructure():
 	mkdir(HIRES_IMG_DIR)
+	mkdir(RAW_HTML_DIR)	
 
 def dl_hires_img(hires_img_url, img_id):
+	#TODO: group them in folders of 100
 	urllib.urlretrieve(hires_img_url, HIRES_IMG_DIR + '/' + img_id + '.tif')
+
+def store_raw_html(id, html):
+	#TODO: group them in folders of 100
+	fp = open(RAW_HTML_DIR + '/' + str(id), 'w')
+	fp.write(html)
 
 #def dl_all_hires_imgs():
 #	#TODO: write this function (the next line is pseudocode)	
@@ -39,30 +46,22 @@ def scrape_and_parse(id):
 		return FALSE
 
 
-def scrape_and_parse_everything():
-	id=1
-	while True:
-		try:
-			scrape_and_parse(id)
-			id+=1
-		except:
-			break
 
-
-def scrape_everything():
-	id=1
-	mkdir(RAW_HTML_DIR)	
-	#while True:
-	while id <= 10:
-		try:
-			html = cdc_phil_scrape(id)
-			#print html
-			fp = open(RAW_HTML_DIR + '/' + str(id), 'w')
-			fp.write(html)
-			id+=1
-		except:
-			print "uh-oh. trouble getting a page."
-			break
+def cdc_phil_scrape_range(start, end):
+	current = start
+	cookiejar = get_me_a_cookie()
+	while current <= end:
+		html = cdc_phil_scrape(current, cookiejar)
+		# if we didn't get a session error page:
+		if not is_session_expired_page(html):
+			store_raw_html(current, html)
+			#TODO: other stuff with the raw html (parse it, etc)
+			current+=1
+		# if we got a session error page:
+		else:
+			print "got a session error page.  going to grab a new cookie..."
+			cookiejar = get_me_a_cookie()
+			
 
 def store_datum(dict):
     table.execute(dict)
@@ -76,4 +75,32 @@ def test_scrape():
 
     
 if __name__ == '__main__':
+	#cdc_phil_scrape_range(113, 123)
 	test_scrape()
+
+
+
+
+#these methods are deprecated because they don't cleverly keep track of cookies
+#so they make many more http requests than necessary
+#def scrape_and_parse_everything():
+#	id=1
+#	while True:
+#		try:
+#			scrape_and_parse(id)
+#			id+=1
+#		except:
+#			break
+#def scrape_everything():
+#	id=1
+#	mkdir(RAW_HTML_DIR)	
+#	#while True:
+#	while id <= 10:
+#		try:
+#			html = cdc_phil_scrape(id)
+#			#print html
+#			store_raw_html(id, html)
+#			id+=1
+#		except:
+#			print "uh-oh. trouble getting a page."
+#			break
