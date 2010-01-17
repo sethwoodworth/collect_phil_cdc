671dae5660222be3a65182235f583db2ee2f3ec0
Seth Woodworth <seth@isforinsects.com>
2010-01-17 02:02:50 -0500

    reorg meta scripts

--- cleanup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm ./*.pyc
-mv phil.cdc.sql _OLD_DB.SQL
---
+++ scripts/cleanup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# run this from the root of the collect-cdc-phil directory
+
+rm ./*.pyc
+mv phil.cdc.sql _OLD_DB.SQL
