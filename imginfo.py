dd285f3dad662231248d67c6cc7a4733ab2be578
it <it@itd-latitude.olin.edu>
2010-01-17 18:16:13 -0500

    introducing imginfo.py, containing a function which can be passed a list of directories, and will return a list of lists containing path to files, file size (X by Y), format (file type), and mode (RGB for color, L for greyscale)

---
+++ imginfo.py
@@ -0,0 +1,38 @@
+import os, re
+import Image
+# needs PIL
+
+pics = []
+size = []
+format = []
+mode = []
+
+def imginfo(dirlist):
+	for dir in dirlist:
+		items_in_dir = os.listdir(dir)
+#makes a list of the items in dir, a word in dirlist
+		for item in items_in_dir:
+			pathtoitem=dir+'/'+item
+#defines pathtoitem as a direction to a 
+			if os.path.isfile(pathtoitem):
+				try:
+					Image.open(pathtoitem)
+#checks to see if the file located at pathtoitem is an image by attempting to open it with PIL
+					pics.append(pathtoitem)
+#if it's an image, append the path to the 'pics' list
+				except IOError:
+					print "item located at "+pathtoitem+" is unopenable by PIL"
+			else:
+				pathtoitem=dir+'/'+item
+				dirlist.append(pathtoitem)
+	for pic in pics:
+		im = Image.open(pic)
+		size.append(list(im.size))
+		format.append(im.format)
+		mode.append(im.mode)
+	array = [ pics, size, format, mode ]
+	return array
+
+if __name__ == "__main__":
+	dirlist = ['/tmp/pics']
+	print imginfo(dirlist)
