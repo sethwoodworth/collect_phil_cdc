81ae8888e762ff6871827522d48b9afee9934c3b
Parker Phinney <parker.phinney@gmail.com>
2011-04-01 10:18:23 -0400

    trimming outdated stuff from config

--- config-sample.py
+++ config-sample.py
@@ -7,14 +7,11 @@ RAW_HTML_DIR = DATA_ROOT + 'raw-html'
 # number of downloaders to run
 MAX_DAEMONS = 50
 
-wp_db_host = 'localhost'
-wp_db_db = '_wp'
-wp_db_user = 'phil'
-wp_db_pass = 'toast'
 data_mysql_db_host = 'localhost'
 data_mysql_db_db = 'cdc_phil_data'
 data_mysql_db_user = 'phil'
 data_mysql_db_pass = 'toast'
+
 data_sqlite_db_host = ''
 data_sqlite_db_db = DATA_ROOT + 'metadata.sqlite'
 data_sqlite_db_user = ''
